4287bbb8dfe3284fbe3f510fd13056d44495b6d0
Nicolas Young <nicolaskyoung@gmail.com>
2020-08-30 13:21:15 -0700

    PHP test again

--- senior-portrait-requests.php
+++ senior-portrait-requests.php
@@ -1,14 +1,8 @@
-## Senior Portrait Request Form
-
-I know how much my friends *loved* filling out their college application forms, so I've decided to emulate the experience. If you want a senior portrait, please fill out the following form!
-
-\* required field
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-Name: <input type="text" id="name" name="name"/>*
-E-mail: <input type="text" id="name" name="email"/>*
-Address: <input type="text" id="name" name="address"/>*
-<input type="submit">
-</form>
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>  
 
 <?php
 // define variables and set to empty values
@@ -28,4 +22,35 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+?>
+
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
 ?>
