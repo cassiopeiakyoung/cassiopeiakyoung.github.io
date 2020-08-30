178af7ee13334b1177cbe35fdac567fbdd03cec2
Nicolas Young <nicolaskyoung@gmail.com>
2020-08-30 13:47:13 -0700

    PHP test 5

--- acceptportraits.php
+++ acceptportraits.php
@@ -4,8 +4,12 @@ $name = $_POST[filter_var($str, 'name')];
 $email = $_POST[filter_var($email, 'email')];
 $address = $_POST[filter_var($str, 'address')];
 
-$email_from = 'nicolaskyoung@gmail.com';
+$email_from = $email;
 $email_subject = "New Form submission";
 $email_body = "You have received a form submission from the user $name. Their email address is $email, and their address is $address\n".
+
+$to = "nicolaskyoung@gmail.com";
+$headers = "From: $email_from \r\n";
+mail($to,$email_subject,$email_body,$headers);
 ?>
 
